65487c8cd391c58398d0b3f82455f1515ab4403c
ShipleyC95 <39995399+ShipleyC95@users.noreply.github.com>
2018-08-03 13:19:10 +0100

    2.2

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -13,7 +13,7 @@ class QuizQuestion extends Component
 					</section>
 					<section className="buttons">
 						<ul>
-							<li> this.props.quiz_questions.answer_options[0] </li>
+							<li>{this.props.quiz_questions.answer_options[0]} </li>
 						</ul>
 					</section>
 			</main>
