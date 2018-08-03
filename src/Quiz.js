834ff21455cd5eb20af1e66784ced0c824529533
ShipleyC95 <39995399+ShipleyC95@users.noreply.github.com>
2018-08-03 14:53:45 +0100

    3.3

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,8 +16,13 @@ class Quiz extends Component {
 		
 		return(
 		<div>
-			<QuizEnd />
-			<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1 ]}/>
+			if(isQuizEnd === true)
+			{<QuizEnd />}
+			else
+			{
+				<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1 ]}/>
+			}
+			
 		</div>
 		)
 	}
