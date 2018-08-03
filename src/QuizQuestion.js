ea3c72b2d1fddb192acd71fe21ee6f10f7f35657
ShipleyC95 <39995399+ShipleyC95@users.noreply.github.com>
2018-08-03 16:26:27 +0100

    4.1

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -15,7 +15,7 @@ class QuizQuestion extends Component
 					<section className="buttons">
 						// <ul>
 							{this.props.quiz_question.answer_options.map((answer_option, index) => {
-								return <QuizQuestionButton key = {index}  button_text={answer_option} clickHandler = {this.handleClick.bind(this)}/>
+								return <QuizQuestionButton key = {index}  button_text={answer_option} clickHandler = {this.handleClick.bind(this)} />
 							})}
 							
 						</ul>
@@ -25,7 +25,9 @@ class QuizQuestion extends Component
 	}	
 	
 	handleClick(buttonText) {
-	
+		if(buttonText === this.props.quiz_question.answer) {
+				this.props.showNextQuestionHandler()
+		}
 	}
 	
 }
