39590568dfa7311f41560a2341962b7e89624510
ShipleyC95 <39995399+ShipleyC95@users.noreply.github.com>
2018-08-03 14:32:38 +0100

    3.3

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react'
 
 class QuizQuestionButton extends Component
 {
-	render()
-	{
-		return
-		(
-				<li>
-					<button>button_text</button>
-				</li>
+	render() {
+		return (
+		<li> <button> button_text </button> </li>
 		)
 	}
 }
