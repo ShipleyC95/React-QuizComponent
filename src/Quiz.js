531e4688200abf307c009c188e32dfe291795358
ShipleyC95 <39995399+ShipleyC95@users.noreply.github.com>
2018-08-03 14:46:13 +0100

    3.3

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,12 +4,16 @@ import QuizQuestion from './QuizQuestion.js'
 import QuizEnd from './QuizEnd.js'
 let quizData = require('./quiz_data.json')
 
+
+
 class Quiz extends Component {
 	constructor(props){
 	super(props)
 	this.state = {quiz_position: 1}
 	}
 	render(){
+		const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length)
+		
 		return(
 		<div>
 			<QuizEnd />
