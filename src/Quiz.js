774ccabf02a47f7f6be843365914d8928248cef1
ShipleyC95 <39995399+ShipleyC95@users.noreply.github.com>
2018-08-03 15:32:21 +0100

    3.3

--- src/Quiz.js
+++ src/Quiz.js
@@ -13,20 +13,23 @@ class Quiz extends Component {
 	}
 	render(){
 		const isQuizEnd = true //(this.state.quiz_position - 1 === quizData.quiz_questions.length)
-		
-		return(
-		<div>
-			if(isQuizEnd === true)
-			{
+					
+		if(isQuizEnd)
+		{
+			return(
+			<div>
 				<QuizEnd />
-			}
-			else if(isQuizEnd === false)
-			{
+			</div>
+			);
+		}
+		else
+		{
+			return(
+			<div>
 				<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1 ]}/>
-			}
-			
-		</div>
-		)
+			</div>
+			);
+		}
 	}
 }
 
